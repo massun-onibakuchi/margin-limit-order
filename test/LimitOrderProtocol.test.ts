48d5df4f98d6335ea6d4daee06805622f16f451b
massun-onibakuchi <etherdao0x00@gmail.com>
2021-09-21 00:37:39 +0900

    test:fix encodeFunction,

--- test/LimitOrderProtocol.test.ts
+++ test/LimitOrderProtocol.test.ts
@@ -1,7 +1,13 @@
 import { deployments, ethers, getNamedAccounts } from "hardhat"
 import { expect, use } from "chai"
-import { Contract } from "ethers"
-import { WrappedTokenMock, TokenMock, LimitOrderProtocol, InteractiveNotificationReceiverMock } from "../typechain"
+import { BigNumberish, Contract } from "ethers"
+import {
+    WrappedTokenMock,
+    TokenMock,
+    LimitOrderProtocol,
+    InteractiveNotificationReceiverMock,
+    IERC20,
+} from "../typechain"
 import { buildOrderData, cutLastArg } from "./helpers/utils"
 
 use(require("chai-bignumber")())
@@ -14,16 +20,16 @@ describe("LimitOrderProtocol", async function () {
     const zeroAddress = ethers.constants.AddressZero
 
     function buildOrder(
-        exchange,
-        makerAsset,
-        takerAsset,
-        makerAmount,
-        takerAmount,
+        exchange: LimitOrderProtocol,
+        makerAsset: IERC20,
+        takerAsset: IERC20,
+        makerAmount: BigNumberish,
+        takerAmount: BigNumberish,
         taker = zeroAddress,
         predicate = "0x",
         permit = "0x",
         interaction = "0x",
-        customReciever,
+        customReciever?: string,
     ) {
         return buildOrderWithSalt(
             exchange,
@@ -41,30 +47,30 @@ describe("LimitOrderProtocol", async function () {
     }
 
     function buildOrderWithSalt(
-        exchange,
-        salt,
-        makerAsset,
-        takerAsset,
-        makerAmount,
-        takerAmount,
+        exchange: LimitOrderProtocol,
+        salt: string,
+        makerAsset: IERC20,
+        takerAsset: IERC20,
+        makerAmount: BigNumberish,
+        takerAmount: BigNumberish,
         taker = zeroAddress,
         predicate = "0x",
         permit = "0x",
         interaction = "0x",
-        customReciever,
+        customReciever?,
     ) {
         const receiver = customReciever === undefined ? wallet : customReciever
         return {
             salt: salt,
             makerAsset: makerAsset.address,
             takerAsset: takerAsset.address,
-            makerAssetData: makerAsset.contract.methods.transferFrom(wallet, taker, makerAmount).encodeABI(),
-            takerAssetData: takerAsset.contract.methods.transferFrom(taker, receiver, takerAmount).encodeABI(),
+            makerAssetData: makerAsset.interface.encodeFunctionData("transferFrom", [wallet, taker, makerAmount]),
+            takerAssetData: takerAsset.interface.encodeFunctionData("transferFrom", [taker, receiver, takerAmount]),
             getMakerAmount: cutLastArg(
-                exchange.contract.methods.getMakerAmount(makerAmount, takerAmount, 0).encodeABI(),
+                exchange.interface.encodeFunctionData("getMakerAmount", [makerAmount, takerAmount, 0]),
             ),
             getTakerAmount: cutLastArg(
-                exchange.contract.methods.getTakerAmount(makerAmount, takerAmount, 0).encodeABI(),
+                exchange.interface.encodeFunctionData("getTakerAmount", [makerAmount, takerAmount, 0]),
             ),
             predicate: predicate,
             permit: permit,
@@ -77,14 +83,15 @@ describe("LimitOrderProtocol", async function () {
     let weth: WrappedTokenMock
     let swap: LimitOrderProtocol
     let notificationReceiver: InteractiveNotificationReceiverMock
-    const { wallet, receiver } = await getNamedAccounts()
-    const signer = await ethers.getSigner(wallet)
+    const { owner: ownerAddr, wallet, receiver } = await getNamedAccounts()
+    const signer1 = await ethers.getSigner(wallet)
+    const owner = await ethers.getSigner(ownerAddr)
+    const deployedContracts: { [name: string]: Contract } = {}
 
     beforeEach(async function () {
-        const deployedContracts: { [name: string]: Contract } = {}
         const results = await deployments.fixture(["LimitOrderProtocol"])
         for (const [name, result] of Object.entries(results)) {
-            deployedContracts[name] = await ethers.getContractAt(name, result.address, signer)
+            deployedContracts[name] = await ethers.getContractAt(name, result.address, owner)
         }
         ;({
             LimitOrderProtocol: swap,
@@ -93,9 +100,6 @@ describe("LimitOrderProtocol", async function () {
             InteractiveNotificationReceiverMock: notificationReceiver,
         } = deployedContracts as any)
 
-        // We get the chain id from the contract because Ganache (used for coverage) does not return the same chain id
-        // from within the EVM as from the JSON RPC interface.
-        // See https://github.com/trufflesuite/ganache-core/issues/515
         chainId = await dai.getChainId()
 
         await dai.mint(wallet, "1000000")
@@ -103,47 +107,45 @@ describe("LimitOrderProtocol", async function () {
         await dai.mint(receiver, "1000000")
         await weth.mint(receiver, "1000000")
 
-        await dai.approve(swap.address, "1000000")
-        await weth.approve(swap.address, "1000000")
-        await dai.approve(swap.address, "1000000", { from: wallet })
-        await weth.approve(swap.address, "1000000", { from: wallet })
+        await dai.connect(await ethers.getSigner(account.address)).approve(swap.address, "1000000")
+        await weth.connect(await ethers.getSigner(account.address)).approve(swap.address, "1000000")
+        await dai.connect(signer1).approve(swap.address, "1000000")
+        await weth.connect(signer1).approve(swap.address, "1000000")
     })
 
-    describe("Interaction", async function () {
-        it("should fill and unwrap token", async function () {
-            const amount = toWei("1")
-            await signer.sendTransaction({ to: weth.address, value: amount })
+    it("Interaction - should fill and unwrap token", async function () {
+        const amount = toWei("1")
+        await signer1.sendTransaction({ to: weth.address, value: amount })
 
-            const interaction = notificationReceiver.address + wallet.substr(2)
+        const interaction = notificationReceiver.address + wallet.substr(2)
 
-            const order = buildOrder(
-                swap,
-                dai,
-                weth,
-                1,
-                1,
-                zeroAddress,
-                swap.interface.encodeFunctionData("timestampBelow", [0xff00000000]),
-                "0x",
-                interaction,
-                notificationReceiver.address,
-            )
-            const data = buildOrderData(chainId, swap.address, order)
-            const signature = await account._signTypedData(data.domain, data.types, data.message)
+        const order = buildOrder(
+            swap,
+            dai,
+            weth,
+            1,
+            1,
+            zeroAddress,
+            swap.interface.encodeFunctionData("timestampBelow", [0xff00000000]),
+            "0x",
+            interaction,
+            notificationReceiver.address,
+        )
+        const data = buildOrderData(chainId, swap.address, order)
+        const signature = await account._signTypedData(data.domain, data.types, data.message)
 
-            const makerDai = await dai.balanceOf(wallet)
-            const takerDai = await dai.balanceOf(receiver)
-            const makerWeth = await weth.balanceOf(wallet)
-            const takerWeth = await weth.balanceOf(receiver)
-            const makerEth = await ethers.provider.getBalance(wallet)
+        const makerDai = await dai.balanceOf(wallet)
+        const takerDai = await dai.balanceOf(receiver)
+        const makerWeth = await weth.balanceOf(wallet)
+        const takerWeth = await weth.balanceOf(receiver)
+        const makerEth = await ethers.provider.getBalance(wallet)
 
-            await swap.fillOrder(order, signature, 1, 0, 1)
+        await swap.fillOrder(order, signature, 1, 0, 1)
 
-            expect(await dai.balanceOf(wallet)).to.equal(makerDai.sub(1))
-            expect(await dai.balanceOf(receiver)).to.equal(takerDai.add(1))
-            expect(await weth.balanceOf(wallet)).to.equal(makerWeth)
-            expect(await ethers.provider.getBalance(wallet)).to.equal(makerEth.add(1))
-            expect(await weth.balanceOf(receiver)).to.equal(takerWeth.sub(1))
-        })
+        expect(await dai.balanceOf(wallet)).to.equal(makerDai.sub(1))
+        expect(await dai.balanceOf(receiver)).to.equal(takerDai.add(1))
+        expect(await weth.balanceOf(wallet)).to.equal(makerWeth)
+        expect(await ethers.provider.getBalance(wallet)).to.equal(makerEth.add(1))
+        expect(await weth.balanceOf(receiver)).to.equal(takerWeth.sub(1))
     })
 })
